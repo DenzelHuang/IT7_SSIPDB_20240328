e36d77db62eb67dbe968ce7809f25dea2c9e1988
MILO-05 <diegoradityas11@gmail.com>
2024-03-30 07:42:32 +0700

    ADD CSS

--- WMS/resources/views/header.blade.php
+++ WMS/resources/views/header.blade.php
@@ -1,3 +1,4 @@
+<link href="{{ asset('css/Stylesheet.css') }}" rel="stylesheet">
 <div>
     <h1>Header</h1>
 </div>
--- WMS/resources/views/login.blade.php
+++ WMS/resources/views/login.blade.php
@@ -1,3 +1,4 @@
+<link href="{{ asset('css/Stylesheet.css') }}" rel="stylesheet">
 <div>
     @include('header')
     <h3>Login Page</h3>
