83ce96cc5659266b2d06be88953fc77864016610
Spengobeb <147594514+Spengobeb@users.noreply.github.com>
2024-03-30 21:13:04 +0700

    Rebuilt for Bootstrap 5

--- WMS/resources/views/home.blade.php
+++ WMS/resources/views/home.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Header</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    @include('header')
+
+    <h1 class="text-center mx">
+        Home Page
+    </h1>
+    @include('footer')
+    
+</body>
+</html>
+
