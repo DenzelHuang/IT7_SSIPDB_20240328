67e3f846ae980a0c3b89099316cb72d4611f2050
Spengobeb <147594514+Spengobeb@users.noreply.github.com>
2024-03-30 19:56:28 +0700

    Small changes

--- WMS/routes/web.php
+++ WMS/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
 
 Route::get("/greet", [AccountController::class, "greet"]);
