e8abb4c79ff4a41e0095c72b30d9441869651975
Spengobeb <147594514+Spengobeb@users.noreply.github.com>
2024-04-02 15:25:39 +0700

    Login page WIP
    Can't figure out how to center the div.

--- WMS/resources/views/account/login.blade.php
+++ WMS/resources/views/account/login.blade.php
@@ -6,32 +6,47 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <style>
+        /* Some custom styles to beautify this example */
+        .bd-highlight{
+            background: #dbdfe5;
+            min-height: 300px;
+        }
+        .box{
+            padding: 25px;
+            background: #abb1b8;
+        }
+    </style>
 </head>
 <body>
-    <main class="form-signin w-25 m-auto align-middle">
-        <form>
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-        
-            <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
-            </div>
-            <br>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-            </div>
-        
-            <div class="form-check text-start my-3">
-                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-                <label class="form-check-label" for="flexCheckDefault">
-                Remember me
-                </label>
-            </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-            <p class="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
-        </form>
-    </main>
+    <div class="m-3" style="">
+        <div class="d-flex align-items-center bd-highlight">
+            <div class="box w-100">Vertical Center Aligned Div</div>
+        </div>
+    </div>
+        {{-- <form>
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    
+        <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+        <br>
+        <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+        </div>
+    
+        <div class="form-check text-start my-3">
+            <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+            Remember me
+            </label>
+        </div>
+        <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+    </form> --}}
+
 </body>
 </html>
 
