d8dc5411aa7abc730b09af1580337c181b6a49fd
Spengobeb <147594514+Spengobeb@users.noreply.github.com>
2024-04-27 15:34:24 +0700

    Minor changes

--- WMS/resources/views/product/index.blade.php
+++ WMS/resources/views/product/index.blade.php
@@ -1,5 +1,5 @@
 @extends('header')
-@section('title', 'Product')
+@section('title', 'Products')
 @section('product_active', 'active')
 @section('scripts')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
